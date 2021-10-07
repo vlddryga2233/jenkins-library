66a1608895a65d5a3bdc1b03bdf73daadbce8fe3
vlddryga2233 <vlddryga@gmail.com>
2021-10-07 18:53:34 +0300

    test

--- vars/pingServer.groovy
+++ vars/pingServer.groovy
@@ -1,5 +1,5 @@
 def call(List servers){  
-            for(int i in servers.size())
+            for(int i in servers)
             {
                 echo "ping ${servers.get(i)}"    
                 sh "ping  ${servers.get(i)} -c 5"
