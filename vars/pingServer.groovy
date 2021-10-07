eefc4d04ee91b63b8b2d673e96579dd4ec2627be
vlddryga2233 <vlddryga@gmail.com>
2021-10-07 14:52:33 +0300

    edit label function

--- vars/pingServer.groovy
+++ vars/pingServer.groovy
@@ -10,17 +10,17 @@ def call(List servers, String nodeLabel){
                 parallel {
                     stage('Ping ${server[0]}') {
                             steps {
-                                sh "ping  ${assert server[0]} -c 5"
+                                sh "ping  ${assert server.get(0)} -c 5"
                             }
                     }
                     stage('Ping ${server[1]}') {
                             steps {
-                                sh "ping ${assert server[1]} -c 5"
+                                sh "ping ${assert server.get(1)} -c 5"
                             }
                     }
                     stage('Ping ${server[2]}') {
                             steps {
-                                sh "ping ${assert server[2]} -c 5"
+                                sh "ping ${assert server.get(2)} -c 5"
                             }
                     }
                 }   
