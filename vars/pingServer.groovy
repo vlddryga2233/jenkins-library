784ec3a2b8a24de7af6505a84a5becd4b7c5da9a
vlddryga2233 <vlddryga@gmail.com>
2021-10-07 18:57:37 +0300

    test

--- vars/pingServer.groovy
+++ vars/pingServer.groovy
@@ -1,7 +1,9 @@
 def call(List servers){  
             for(int i in servers.size()-1)
             {
-                echo "ping ${servers.get(i)}"    
-                sh "ping  ${servers.get(i)} -c 5"
+                parallel(
+                    i : {echo "ping ${servers.get(i)}"    
+                sh "ping  ${servers.get(i)} -c 5"}
+                )    
             } 
 }
