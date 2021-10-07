78924cb4f15970837b6d4274b46e3e1611081614
vlddryga2233 <vlddryga@gmail.com>
2021-10-07 16:06:23 +0300

    edit var

--- vars/archiveBuild.groovy
+++ vars/archiveBuild.groovy
@@ -1,5 +1,5 @@
 def call(){
-    sh 'sudo zip target/${GIT_BRANCH}-demo-app-${OWNER}-${BUILD_NUMBER}.zip target/*.jar'
+    sh 'sudo zip target/main-demo-app-${OWNER}-${BUILD_NUMBER}.zip target/*.jar'
     archiveArtifacts artifacts: 'target/*.zip', fingerprint: true
     echo 'Archived successful'
 }
